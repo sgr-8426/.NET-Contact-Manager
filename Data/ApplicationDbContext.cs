e6be229486bee9170f9d1ac80fd4a27365aed777
sagar <sagarnarigara69@gmail.com>
2025-09-20 02:26:38 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContactManager.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Contact> Contacts { get; set; }  // maps to Contacts table
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+
+        [Required, EmailAddress, RegularExpression(@"^[\w\.-]+@[\w\.-]+\.\w{2,4}$", ErrorMessage = "Please Enter Correct Email Address")]
+        public string Email { get; set; }
+
+        [Phone, MinLength(10, ErrorMessage = "Contact Number Must Be of 10 Digits")]
+        public string Phone { get; set; }
+    }
+}
